69a0404766c9ee4f9b9bd2f932eae0ef1df1b450
Sebastian Wyngaard <swyngaar@nd.edu>
2020-01-30 14:03:56 +0000

    feat: add LANDRS theme

--- setup/migration-helpers.js
+++ setup/migration-helpers.js
@@ -8,24 +8,24 @@ function getConfigFromEnvVars () {
   catch (err) {
     editorConfig = {}
   }
-
+  const DEFAULT_PROJECT_TITLE = 'LANDRS Ontology Manager'
   const editor = {
     loginStrategy: process.env.AUTH_STRATEGY || 'github',
     host: process.env.EDITOR_HOST || 'localhost:3000',
     protocol: process.env.EDITOR_PROTOCOL || 'http',
     meta: {
-      title: _.get(editorConfig, 'head.title', 'Zazuko Ontology Manager Demo'),
-      customerName: process.env.CUSTOMER_NAME || 'Zazuko GmbH',
+      title: _.get(editorConfig, 'head.title', DEFAULT_PROJECT_TITLE),
+      customerName: process.env.CUSTOMER_NAME || 'University of Notre Dame and Zazuko GmbH',
       description: _.get(editorConfig, 'head.description', 'Linked Data Ontology Manager for Domain Specialists')
     },
-    logoUrl: '/zazuko-logo.svg',
+    logoUrl: '/landrs-logo-full.svg',
     text: {
       groupName: 'Container',
       home: [
         '<p>The Ontology Manager is based on a web interface and Github for repository functions. It has been created in order to enable collaboration on schema definition beyond the developer community and enabling the business process experts to engage and drive the creation of the best possible schema for the logistics industry, while ensuring a correct schema definition in Turtle (Terse RDF Triple Language) format.</p>',
         '<p>The Ontology Manager consists of the following functions; </p> <ul class="dash-bullet"> <li> repository of schema in Turtle format (Github) </li> <li> logical visualization of schema </li> <li> forum capability </li> <li> proposal, voting, and acceptance of new schema entries, changes, and deprecation </li> <li> search in all defined schemas </li> <li> activity list view </li> </ul> <p> In order to join you will need a Github account. </p>'
       ],
-      login: `${process.env.CUSTOMER_NAME} uses GitHub as a collaboration platform for the ontology management. Therefore you require a GitHub account to collaborate on ${process.env.CUSTOMER_NAME}.`
+      login: `${process.env.CUSTOMER_NAME} uses GitHub as a collaboration platform for the ontology management. Therefore you require a GitHub account to collaborate on ${_.get(editorConfig, 'head.title', DEFAULT_PROJECT_TITLE)}.`
     },
     github: {
       repo: _.get(editorConfig, 'github.repo', process.env.GITHUB_REPO || 'o'),
@@ -35,7 +35,7 @@ function getConfigFromEnvVars () {
     },
     committer: {
       name: _.get(editorConfig, 'committer.name', 'Ontology Manager'),
-      email: _.get(editorConfig, 'committer.email', 'victor.felder@zazuko.com')
+      email: _.get(editorConfig, 'committer.email', 'landrs@nd.edu')
     }
   }
 
